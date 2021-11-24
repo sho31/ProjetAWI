ce9ba8ff161b89c29d98222f4251cfd101f2cabc
theotheo <theo.uriot@gmail.com>
2021-11-24 17:57:52 +0100

    models

---
+++ backend/models/author.js
@@ -0,0 +1,61 @@
+const db = require("../dataBase/index");
+
+/* CRUD */
+
+async function createAuthor(prenomAuteur, nomAuteur) {
+  try {
+    const res = await db.query(
+      "INSERT INTO Auteur (prenomAuteur, nomAuteur) VALUES($1, $2);",
+      [prenomAuteur, nomAuteur]
+    );
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function updateAuthor(id, prenomAuteur, nomAuteur) {
+  try {
+    const res = await db.query("UPDATE Auteur SET prenomAuteur = $1, nomAuteur = $2 WHERE idAuteur = $3;",
+        [prenomAuteur, nomAuteur, id]);
+    return res;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+async function deleteAuthor(id) {
+  try {
+    const res = await db.query("DELETE FROM Auteur WHERE idAuteur = $1;", [id]);
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getAllAuthors() {
+  try {
+    const res  = await db.query('SELECT * FROM auteur;')
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getAuthorByID(id) {
+  try {
+    const res = await db.query('SELECT * FROM auteur WHERE idauteur=$1',[id])
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+module.exports = {
+  createAuthor,
+  updateAuthor,
+  deleteAuthor,
+  getAllAuthors,
+  getAuthorByID,
+};
---
+++ backend/models/ingredientCat.js
@@ -0,0 +1,69 @@
+const db = require("../dataBase");
+
+/* CRUD */
+
+async function createIngredientCat(nomCategorieIngredient) {
+  try {
+    const res = await db.query(
+      "INSERT INTO CategorieIngredient (nomCategorieIngredient) VALUES($1);",
+      [nomCategorieIngredient]
+    );
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function updateIngredientCat(id, nomCategorieIngredient) {
+  try {
+    const res = await db.query(
+      "UPDATE CategorieIngredient SET nomCategorieIngredient = $1 WHERE idCategorieIngredient = $2;",
+      [nomCategorieIngredient, id]
+    );
+    return res
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+async function deleteIngredientCat(id) {
+  try {
+    const res = await db.query(
+      "DELETE FROM CategorieIngredient WHERE idCategorieIngredient = $1;",
+      [id]
+    );
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getAllIngredientCats() {
+  try {
+    const res = await db.query("SELECT * FROM CategorieIngredient;");
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getIngredientCatByID(id) {
+  try {
+    const res = await db.query(
+      "SELECT * FROM CategorieIngredient WHERE idCategorieIngredient = $1;",
+      [id]
+    );
+    return res;
+  } catch (e) {
+    throw e;
+  }
+}
+
+module.exports = {
+  createIngredientCat,
+  updateIngredientCat,
+  deleteIngredientCat,
+  getAllIngredientCats,
+  getIngredientCatByID,
+};
