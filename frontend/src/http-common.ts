1348afa2b441b01338924082cf921f6f2a43323a
jz_31 <friizyy@gmail.com>
2022-01-01 22:24:00 +0100

    change structure to deploy to heroku - risk of breaking the app

--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
-
+const devConfig ="http://localhost:5000/"
+const proConfig =""
+console.log("config", process.env.NODE_ENV)
 export default axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: process.env.NODE_ENV === "production" ? proConfig : devConfig ,
     headers: {
         "Content-type": "application/json"
     }
