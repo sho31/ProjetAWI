f9b1112c05a441d74467a4979f1820aed7afa592
sho31 <67512241+sho31@users.noreply.github.com>
2022-01-03 15:09:17 +0100

    deploy adjustment

--- backend/dataBase/index.js
+++ backend/dataBase/index.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 const Pool  = require("pg").Pool;
 
-const pool = new Pool({
-  user: process.env.dbUSER,
-  password: process.env.dbPASSWORD,
-  host: process.env.dbHOST,
-  port: process.env.dbPORT,
-  database: process.env.dbDATABASE,
-});
+// const pool = new Pool({
+//   user: process.env.dbUSER,
+//   password: process.env.dbPASSWORD,
+//   host: process.env.dbHOST,
+//   port: process.env.dbPORT,
+//   database: process.env.dbDATABASE,
+// });
 
+const devConfig = `postgresql://${process.env.dbUSER}:${process.env.dbPASSWORD}@${process.env.dbHOST}:${process.env.dbPORT}/${process.env.dbDATABASE}`;
+
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
+const pool = new Pool({
+  connectionString:
+      process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
-
+import * as process from "process";
+let url : string=""
+console.log(process.env.NODE_ENV)
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    url = "http://localhost:4442/"
+} else {
+    url = "https://projet-awi-sheets.herokuapp.com/"
+    //production
+}
 export default axios.create({
-    baseURL: "http://localhost:4442/",
+    baseURL: url,
     headers: {
         "Content-type": "application/json"
     }
