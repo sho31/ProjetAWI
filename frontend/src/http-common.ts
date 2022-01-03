9064a96c77d0cc34f385a944d838be41e77cf3f2
sho31 <67512241+sho31@users.noreply.github.com>
2022-01-03 15:32:27 +0100

    http change

--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import * as process from "process";
 let url : string=""
 console.log(process.env.NODE_ENV)
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    url = "http://localhost:4442/"
-} else {
+if (process.env.NODE_ENV == "production") {
     url = "https://projet-awi-sheets.herokuapp.com/"
+} else {
+    url = "http://localhost:4442/"
     //production
 }
 export default axios.create({
